ee3f892762671728f9620333595b5a44d7ed5f34
gooseandmegander <meganawilliamson@gmail.com>
2020-05-10 10:36:55 -0400

    Solution to exercise 1

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Hello, World!</Text>
+      <View style={layout.container}>
+        <Text style={typography.heading}>
+          Here are some boxes of different colors
+        </Text>
+        <View style={[styles.box, themes.cyan]}>
+          <Text style={typography.boxText}>Cyan: #2aa198</Text>
+        </View>
+        <View style={[styles.box, themes.blue]}>
+          <Text style={typography.boxText}>Blue: #268bd2</Text>
+        </View>
+        <View style={[styles.box, themes.magenta]}>
+          <Text style={typography.boxText}>Magenta: #d33682</Text>
+        </View>
+        <View style={[styles.box, themes.orange]}>
+          <Text style={typography.boxText}>Orange: #cb4b16</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
 }
+
+const layout = StyleSheet.create({
+  container: { paddingHorizontal: 15, paddingVertical: 10 },
+});
+
+const typography = StyleSheet.create({
+  heading: { fontWeight: 'bold', fontSize: 16, paddingVertical: 4 },
+  boxText: { color: 'white', fontWeight: '500' },
+});
+
+const styles = StyleSheet.create({
+  box: {
+    borderRadius: 2,
+    marginVertical: 6,
+    paddingVertical: 8,
+    alignItems: 'center',
+  },
+});
+
+const themes = StyleSheet.create({
+  cyan: { backgroundColor: '#2aa198' },
+  blue: { backgroundColor: '#268bd2' },
+  magenta: { backgroundColor: '#d33682' },
+  orange: { backgroundColor: '#cb4b16' },
+});
