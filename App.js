8c5d8a3d05c52c51af5080f3e54a9d95dc26a9a9
gooseandmegander <meganawilliamson@gmail.com>
2020-05-10 08:25:40 -0400

    Initial commit
    Sets up React Native app using Expo.
    
    Includes:
    - Prettier config
    - Eslint config

--- App.js
+++ App.js
@@ -1,19 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <Text style={styles.desc}>Here are some boxes of different colours</Text>
+      <View style={styles.container}>
+        <Text style={[styles.allText, styles.textCyan]}>Cyan: #2aa198</Text>
+        <Text style={[styles.allText, styles.textBlue]}>Blue: #268bd2</Text>
+        <Text style={[styles.allText, styles.textMagenta]}>Magenta: #d33682</Text>
+        <Text style={[styles.allText, styles.textOrange]}>Orange: #cb4b16</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeArea: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // borderColor: 'yellow',
+    // borderWidth: 2,
+    marginHorizontal: 20,
   },
+  desc: {
+    fontWeight: 'bold',
+  },
+  container: {
+    // borderColor: 'pink',
+    // borderWidth: 2,
+    justifyContent: 'space-between',
+  },
+  allText: {
+    color: '#fff',
+    padding: 10,
+    textAlign: 'center',
+    marginVertical: 5,
+  },
+  textCyan: { backgroundColor: '#2aa198' },
+  textBlue: { backgroundColor: '#268bd2' },
+  textMagenta: { backgroundColor: '#d33682' },
+  textOrange: { backgroundColor: '#cb4b16' },
 });
