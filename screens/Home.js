c176166d538dce5b5c8b46a0bad12b178ab8b0c2
gooseandmegander <meganawilliamson@gmail.com>
2020-05-17 14:38:38 -0400

    Updates the app to fetch color palettes from an API
    - Uses React Hooks: useState, useCallback, and useEffect
    - Uses RefreshControl so users may refresh the color palettes on
      demand
    - A Timeout on the refresh increases UX value

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import { Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react';
+import {
+  Text,
+  FlatList,
+  TouchableOpacity,
+  RefreshControl,
+  StyleSheet,
+} from 'react-native';
 
 import PalettePreview from '../components/PalettePreview';
-import { COLOR_PALETTES, PREVIEW_COUNT } from '../constants';
+import { PREVIEW_COUNT } from '../constants';
 
 export default function Home({ navigation }) {
+  const [colorPalettes, setColorPalettes] = useState([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+
+  const fetchColorPalettes = useCallback(async () => {
+    const response = await fetch(
+      'https://color-palette-api.kadikraman.now.sh/palettes',
+    );
+    if (response.ok) {
+      const palettes = await response.json();
+      setColorPalettes(palettes);
+    }
+  }, []);
+
+  const handleRefresh = useCallback(() => {
+    setIsRefreshing(true);
+    fetchColorPalettes();
+    setTimeout(() => {
+      setIsRefreshing(false);
+    }, 1000);
+  }, [fetchColorPalettes]);
+
+  useEffect(() => {
+    fetchColorPalettes();
+  }, [fetchColorPalettes]);
+
   return (
     <FlatList
       style={styles.container}
-      data={COLOR_PALETTES}
+      data={colorPalettes}
       keyExtractor={(item) => item.paletteName}
       renderItem={({ item }) => (
         <TouchableOpacity
@@ -23,6 +54,9 @@ export default function Home({ navigation }) {
           <PalettePreview theme={item.colors.slice(0, PREVIEW_COUNT)} />
         </TouchableOpacity>
       )}
+      refreshControl={
+        <RefreshControl refreshing={isRefreshing} onRefresh={handleRefresh} />
+      }
     />
   );
 }
